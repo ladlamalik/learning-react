2b4261d31496930e7d5d029a975d2ff00fe485fa
ladlamalik <ladlamalik.haxor@gmail.com>
2024-04-24 18:29:27 +0500

    React Router

--- 07reactRouter/src/App.tsx
+++ 07reactRouter/src/App.tsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from "./omponents/Header/Header"
-import Footer from "./omponents/Footer/Footer"
 
+import Header from "./components/Header/Header"
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 function App() {
   
 
   return (
     <>
      <Header />
+     <Home />
      <Footer />
     </>
   )
--- 07reactRouter/src/main.tsx
+++ 07reactRouter/src/main.tsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Router, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.tsx'
+import Home from './components/Home/Home.tsx'
+import About from './components/About/About.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
